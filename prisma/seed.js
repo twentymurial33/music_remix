9b60e95feb2315e044c6340c7346e901e51e4e0b
Murial Anindo <murial.anindo@lonett.com>
2024-07-03 13:28:32 -0400

    prisma and db correctly setup

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,12 +1,23 @@
-import { PrismaClient } from "@prisma/client";
-
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-export async function getServerSideProps() {
-  const users = await prisma.user.findMany();
-  return {
-    props: {
-      users,
-    },
-  };
+async function main() {
+  const insertResult = await prisma.user.createMany({
+    data: [
+      { id: 10, email: "toast", name: "bellablue" },
+      { id: 22, email: "sausage", name: "pinkpurple" },
+    ],
+  });
+  console.info(
+    `The prisma database was seeded with ${insertResult.count} items.`
+  );
 }
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
