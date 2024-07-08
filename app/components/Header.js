e7a33e5c8423730ffeff9ff319dfc035ad4ead47
Murial Anindo <murial.anindo@lonett.com>
2024-07-08 11:18:40 -0400

    finish styling header

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,22 +1,69 @@
 import styled from "styled-components";
+import Image from "next/image";
 
 function Header() {
   return (
-    <StyledWrapper>
-      <h1>My Logo</h1>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-        <button>Sign Up</button>
-        <button>Register</button>
-      </ul>
-    </StyledWrapper>
+    <StyledContainer>
+      <StyledWrapper>
+        <Image src="logo.png" alt="Logo" height={40} width={20} />
+        <StyledList>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">Solutions</a>
+          </li>
+          <li>
+            <a href="#">Community</a>
+          </li>
+          <li>
+            <a href="#">Resources</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <StyledSignIn>Sign in</StyledSignIn>
+          </li>
+          <li>
+            <StyledRegister>Register</StyledRegister>
+          </li>
+        </StyledList>
+      </StyledWrapper>
+    </StyledContainer>
   );
 }
 
+const StyledContainer = styled.div`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+`;
 const StyledWrapper = styled.div`
-  background-color: pink;
+  background-color: #ff3366;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
 `;
 
+const StyledList = styled.ul`
+  list-style: none;
+  display: flex;
+  margin: 0;
+  padding: 0;
+`;
+
+const StyledSignIn = styled.button`
+  background-color: lightgray;
+  color: black;
+`;
+
+const StyledRegister = styled.button`
+  background-color: black;
+  color: white;
+`;
 export default Header;
