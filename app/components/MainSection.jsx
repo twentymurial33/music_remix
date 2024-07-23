98c8a3479129dcfbfeb7aee1a5143d5f04924a28
Murial Anindo <murial.anindo@lonett.com>
2024-07-23 09:42:44 -0400

    adding music mp3 files

--- app/components/MainSection.jsx
+++ app/components/MainSection.jsx
@@ -1,12 +1,32 @@
 import MainSectionStyles from './MainSection.css';
-import yogaImage from './path-to-your-image.jpg'; // Update the path to your image
 
+
+  const genres = [
+  { name: 'Rock', url: '/music/rock.mp3' },
+  { name: 'Jazz', url: '/music/jazz.mp3' },
+  { name: 'Classical', url: '/music/classical.mp3' },
+  { name: 'Electronic', url: '/music/electronic.mp3' },
+ 
+];
 const MainSection = () => {
+  const [audio, setAudio] = useState(null);
+
+  const playRandomGenre = () => {
+    if (audio) {
+      audio.pause();
+      setAudio(null);
+    }
+    const randomGenre = genres[Math.floor(Math.random() * genres.length)];
+    const newAudio = new Audio(randomGenre.url);
+
+    newAudio.play();
+    setAudio(newAudio);
+  };
   return (
     <section className="yoga-wear-section">
       <div className="content">
         <h1>Friendly Music & Workouts.</h1>
-        <button className="shop-now-btn">Listen Now</button>
+        <button className="shop-now-btn" onClick={playRandomGenre}>Listen Now</button>
       </div>
       <div className="image-container">
         <img src={yogaImage} alt="Yoga" className="yoga-image" />
